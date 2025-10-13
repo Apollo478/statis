4bf18bee93e8bc7c48c22a9835e69a25af214ce5
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2025-10-13 18:02:53 +0100

    abstracted

--- src/capture.rs
+++ src/capture.rs
@@ -1,4 +1,5 @@
-use x11rb::rust_connection::RustConnection;
+use image::{ImageBuffer, Rgb};
+use x11rb::{connection::Connection, protocol::xproto::{get_image, ImageFormat}, rust_connection::RustConnection};
 
 
 pub struct ScreenCapture {
@@ -11,4 +12,47 @@ pub struct CaptureRegion {
     pub y: u16,
     pub width: u16,
     pub height: u16,
+}
+
+impl ScreenCapture {
+    pub fn new() -> Result<Self,Box<dyn std::error::Error>> {
+        let (conn,screen_num) = x11rb::connect(None)?;
+        Ok(Self { conn: (conn), screen_num: (screen_num) } ) 
+    }
+    
+    pub fn screen_dimensions(&self) -> (u16,u16) {
+        let screen = &self.conn.setup().roots[self.screen_num];
+        (screen.width_in_pixels,screen.height_in_pixels)
+    }
+
+    pub fn capture_region(&self,region: CaptureRegion) -> Result<ImageBuffer<Rgb<u8>,Vec<u8>>,Box<dyn std::error::Error>> {
+        let screen = &self.conn.setup().roots[self.screen_num];
+        let image  =  get_image(&self.conn, ImageFormat::Z_PIXMAP,screen.root, region.x as i16, region.y as i16, region.width, region.height, u32::MAX).expect("Could not get image");
+        let reply = image.reply()?;
+        let rgb = bgrx_to_rgb(reply.data);
+
+        ImageBuffer::from_raw(region.width as u32, region.height as u32, rgb).ok_or_else(|| "Invalid buffer size".into())
+    }
+
+    pub fn capture_full_screen(&self) -> Result<ImageBuffer<Rgb<u8>,Vec<u8>>,Box<dyn std::error::Error>> {
+        let (width, height) = self.screen_dimensions();
+        self.capture_region(CaptureRegion { x: (0), y: (0), width: (width), height: (height) })
+    }
+
+}
+
+
+
+
+fn bgrx_to_rgb(data: Vec<u8>) -> Vec<u8> {
+    let mut rgb = Vec::with_capacity(data.len() / 4 * 3);
+    for chunk in data.chunks_exact(4) {
+        let b = chunk[0];
+        let g = chunk[1];
+        let r = chunk[2];
+        rgb.push(r);
+        rgb.push(g);
+        rgb.push(b);
+    }
+    rgb
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,8 @@
+use crate::capture::ScreenCapture;
+
 mod capture;
-use image::{ImageBuffer, Rgb};
-use x11rb::{connection::Connection, protocol::xproto::{get_image, ImageFormat}};
 fn main() {
-    let (conn,screen_num) = x11rb::connect(None).unwrap();
-    let screen = &conn.setup().roots[screen_num];
-    let height = screen.height_in_pixels as u64;
-    let full_width = screen.width_in_pixels as u64;
-    let capture_x = 1920;
-    let capture_width = (screen.width_in_pixels /2) as u64;
-    let image  =  get_image(&conn, ImageFormat::Z_PIXMAP,screen.root, capture_x, 0, capture_width as u16, screen.height_in_pixels, u32::MAX).expect("Could not get image");
-    let reply = image.reply().expect("could not get reply");
-    let reply_size = reply.data.len();
-    let pix_format = ((screen.root_depth + 8) / 8) as u64;
-    let image_size = (capture_width * height * pix_format)  as usize;
-    assert_eq!(reply_size,image_size);
-    let rgb = bgrx_to_rgb(reply.data);
-
-    let img = ImageBuffer::<Rgb<u8>,_>::from_raw(capture_width as u32, height as u32, rgb).expect("invalid buffer size");
-
-    img.save("ss.png").unwrap();
-
-}
-fn bgrx_to_rgb(data: Vec<u8>) -> Vec<u8> {
-    let mut rgb = Vec::with_capacity(data.len() / 4 * 3);
-    for chunk in data.chunks_exact(4) {
-        let b = chunk[0];
-        let g = chunk[1];
-        let r = chunk[2];
-        rgb.push(r);
-        rgb.push(g);
-        rgb.push(b);
-    }
-    rgb
+    let capture = ScreenCapture::new().expect("failed to get new capture");
+    let fs = capture.capture_full_screen().expect("Could not capture screen");
+    fs.save("ss.png");
 }
